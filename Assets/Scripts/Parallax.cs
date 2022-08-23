5bc06057cd49e5dd8a910b4f219b6c350d7ee2f9
Jay <freddtojay@Live.com>
2022-08-23 16:29:29 -0600

    Added Parallax Effect
    Parallax still in progress. Still have some stuff to tweak, but overall looks nice.
    Still needs work on Character Sprite and Character Collider.

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float multiplier;
+    public GameObject camera;
+    private Vector3 startPostion;
+    private Vector3 velocity = Vector3.zero;
+    public float smoothTime;
+
+    private void Start()
+    {
+        startPostion = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 nextPos = new Vector3(startPostion.x * (multiplier * camera.transform.position.x), transform.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, nextPos, ref velocity, smoothTime);
+    }
+}
