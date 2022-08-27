5794e909c81164b1dc1c35646c69641d9c0fa4f0
Jay <freddtojay@Live.com>
2022-08-27 14:58:02 -0600

    Fixed Parallaxing
    Fixed Parallaxing and added a Stat Script to player.

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Basic Stats")]
+    [SerializeField] private float MaxOxygen;
+    [SerializeField] private float Oxygen;
+    [SerializeField] private float MaxHealth;
+    [SerializeField] private float Health;
+
+    [Header("Components")]
+    public StatBar OxygenStatBar;
+
+
+    
+    
+    private void Awake()
+    {
+        Oxygen = MaxOxygen;
+        Health = MaxHealth;
+        OxygenStatBar.SetValue(Oxygen);
+    }
+
+    private void Update()
+    {
+        
+
+    }
+}
---
+++ Assets/Scripts/Player/StatBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StatBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxValue(float sValue)
+    {
+        slider.maxValue = sValue;
+        slider.value = sValue;
+
+        fill.color = gradient.Evaluate(1f);
+
+    
+    }
+
+    public void SetValue(float sValue)
+    {
+        slider.value = sValue;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
